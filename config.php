46cca6bf268d5b3f6e791e2add80c4080951c215
Glenn Thenu <glenthenu@gmail.com>
2023-09-21 10:43:27 +0700

    tes
    tes

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php $config = array(
+    "database_type"=>"mysql",
+    "database_name"=>"asset",
+    "server"=>"localhost",
+    "username"=>"root",
+    "password"=>"",
+    "charset"=>"utf8",
+    "port"=>3306,
+    "encryption_key"=>"b88588b4c02ccab55a0cf1ac0e6a4334f60fc3b8ba82603c07da9322a06fa61b",
+	"encryption_iv"=>"b98816a9857a30df9954a5a10e1b063ac1c24ea7f76f93445e80e83c7af5169f" ); 
+	?>
